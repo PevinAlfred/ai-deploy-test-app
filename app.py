3988d387c3b3ffced41c1b4e7029dbbbb4b3140a
Pevin Alfred <67494353+PevinAlfred@users.noreply.github.com>
2025-07-21 22:29:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
+import random
 
 app = Flask(__name__)
-# This line creates the /metrics endpoint
+# This automatically creates the /metrics endpoint and tracks requests
 metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# Adding this new route to simulate a problem
+@app.route('/error')
+def trigger_error():
+    """This endpoint will frequently raise an error to test incident response."""
+    # We'll simulate an 80% failure rate for this endpoint
+    if random.random() > 0.2:
+        # This raises a 500 Internal Server Error
+        raise Exception("Simulating a critical database connection error!")
+    
+    return "This time it worked!", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
